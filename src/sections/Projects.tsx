c2cfb0ed9c7b602a3213a17d9b5a568cf4bfc27c
nic-cab-sor <nicole.cabaya.soriano@gmail.com>
2025-12-22 01:36:44 +0000

    hero section tweaks

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -9,13 +9,13 @@ function Projects() {
                 <h2>Projects</h2>
             </header>
             <p>Here are some projects I've worked on.</p>
-            <div className="row row-cols-lg-2">
+            {/* <div className="row row-cols-lg-2">
                 <ProjectCard githubLink={githubPage} type={FRONT_END_BADGE} title="Example Project" description="This is an example project description." skills={[JAVA_BADGE]} />
                 <ProjectCard githubLink={githubPage} type={BACK_END_BADGE} title="title" description="description" skills={[JS_BADGE, PYTHON_BADGE]} />
                 <ProjectCard githubLink={githubPage} type={FULL_STACK_BADGE} title="title" description="description" skills={[JS_BADGE]} />
                 <ProjectCard githubLink={githubPage} type={FRONT_END_BADGE} title="title" description="description" skills={[JAVA_BADGE, JS_BADGE]} />
                 <ProjectCard githubLink={githubPage} type={FRONT_END_BADGE} title="title" description="description" skills={[JS_BADGE, BOOTSTRAP_BADGE]} />
-            </div>
+            </div> */}
         </section>
     )
 }
