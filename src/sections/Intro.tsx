032672115070f12ca4d1da867866169975c50dff
nic-cab-sor <nicole.cabaya.soriano@gmail.com>
2025-12-22 01:50:14 +0000

    hero section tweaks

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -8,7 +8,7 @@ function Intro() {
             </img>
             
             <div className='hero-text d-flex flex-column gap-2 pt-2'>
-                <h1 className="hero-heading fw-bold fst-italic text-center m-0 p-0">NICOLE CABAYA</h1>
+                <h1 className="hero-heading fst-italic text-center m-0 p-0">NICOLE CABAYA</h1>
 
                 <div className='d-inline-flex flex-column flex-sm-row justify-content-center gap-2'>
                     <span className="badge text-bg-info">#Self-motivated</span>
@@ -16,7 +16,7 @@ function Intro() {
                     <span className="badge text-bg-info">#Pro Gamer</span>
                 </div>
 
-                <p className="text-center m-0">Hi! I'm a <span className='fw-bold'>Software Engineer</span>, I write code to make processes easier and users <span className='fw-bold'>happier</span> — <span className='fst-italic'>one line</span> at a time.</p>
+                <p className="text-center">Hi! I'm a <span className='fw-bold'>Software Engineer</span>, I write code to make processes easier and users <span className='fw-bold'>happier</span> — <span className='fst-italic'>one line</span> at a time.</p>
 
                 <div className='d-flex flex-column justify-content-center align-items-center gap-1'>
                     <a href="#projects" className="fw-bold link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Go to Projects</a>
